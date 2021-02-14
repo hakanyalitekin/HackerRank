fc947706717ab5d55637568dc83f045bb3d32abd
Hakan Yalitekin <hyalitekin@tekzen.com.tr>
2021-02-14 14:13:08 +0300

    FizzBuzz added.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz(15);
+            Console.Read();
+        }
+
+        public static void FizzBuzz(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+
+                }
+                else
+                {
+                    Console.WriteLine(i.ToString());
+                }
+            }
+        }
+    }
+}
